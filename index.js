f91b871f4338dc1b985eb67d151acf67c80113f7
deskastudio <mulyanadeska85@gmail.com>
2024-09-25 21:53:37 +0700

    add typescript

---
+++ index.js
@@ -0,0 +1,38 @@
+// let angka = 5;
+
+// if (angka > 0) {
+//     console.log("Angka positif");
+// } else if (angka < 0) {
+//     console.log("Angka negatif");
+// } else {
+//     console.log("Angka nol");
+// }
+
+
+let hari = 7;
+
+switch (hari) {
+    case 1:
+        console.log("Hari ini adalah Senin");
+        break;
+    case 2:
+        console.log("Hari ini adalah Selasa");
+        break;
+    case 3:
+        console.log("Hari ini adalah Rabu");
+        break;
+    case 4:
+        console.log("Hari ini adalah Kamis");
+        break;
+    case 5:
+        console.log("Hari ini adalah Jumat");
+        break;
+    case 6:
+        console.log("Hari ini adalah Sabtu");
+        break;
+    case 7:
+        console.log("Hari ini adalah Minggu");
+        break;
+    default:
+        console.log("Nomor hari tidak valid");
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+let name1: string = "John";
+let age: number = 30;
+let isAdult: boolean = true;
+let pets: number[] = [1, 2, 3];
+let pets2: Array<number> = [1, 2, 3];
+let pets3: any[] = [1, 2, 3];
+let pets4: Array<any> = [1, 2, 3];
+
+console.log(name1, age, isAdult, pets, pets2, pets3, pets4);
